7e857c7a8459546fa1a4fc593df32a3df944e5b0
Gaby Nieva <gaby.nieva@outlook.com>
2023-02-26 22:35:18 -0600

    Segunda parte de reviews

--- lib/screens/review.dart
+++ lib/screens/review.dart
@@ -1,11 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Review extends StatelessWidget {
-  final String pathImage = 'asset/img/waterFalls1';
-   const Review({super.key});
+    final String pathImage;
+    final String user;
+    final String details;
+    final String comment;
+
+    const Review({super.key, required this.user, required this.details, required this.comment, required this.pathImage});
 
   @override
   Widget build(BuildContext context) {    
+
+    final userInfo = Container(
+      margin: const EdgeInsets.only(
+        left: 20.0,
+        ),
+        child: Text(
+          details,
+          textAlign: TextAlign.left,
+          style: const TextStyle(
+            fontFamily: "Lato",
+            fontSize: 13.0,
+            color: Color(0xFFa3a5a7),
+          ),
+
+        ),
+    );
+
+    final userName = Container(
+      margin: const EdgeInsets.only(
+        left: 20.0,
+        ),
+        child: Text(
+          user,
+          textAlign: TextAlign.left,
+          style: GoogleFonts.lato(
+            fontSize: 17.0,
+          ),
+        ),
+    );
+
+    final userComment = Container(
+      margin: const EdgeInsets.only(
+        left: 20.0,
+        ),
+        child: Text(
+          details,
+          textAlign: TextAlign.left,
+          style: GoogleFonts.lato(
+            fontSize: 13.0,
+            color: const Color(0xFFa3a5a7),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+    );
+
+
+    final userDetails = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        userName,
+        userInfo,
+        userComment,
+      ],
+    );
+
     final photo = Container(
       margin: const EdgeInsets.only(
           top: 20.0,
@@ -21,6 +81,11 @@ class Review extends StatelessWidget {
       ),
     );
 
-    return Row();
+    return Row(
+      children: [
+        photo,
+        userComment,
+      ],
+    );
   }
 }
---
+++ lib/screens/review_lists.dart
@@ -0,0 +1,19 @@
+import 'package:basic_app/screens/review.dart';
+import 'package:flutter/material.dart';
+
+class ReviewList extends StatelessWidget{
+  const ReviewList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Review(user: 'Filomena Acosta', details: '1 review 5 photos', comment: 'There is an amazing place in Sri Lanka', pathImage:'asset/img/profile_pic1'),
+        Review(user: 'Pánfilo Torres', details: '3 review 10 photos', comment: 'Beautiful place', pathImage:'asset/img/profile_pic2'),
+        Review(user: 'Tiburcio Luna', details: '1 review 1 photo', comment: 'So peaceful!', pathImage:'asset/img/profile_pic3'),
+        Review(user: 'Anastasia Meléndez', details: '2 review 3 photos', comment: 'Wonderfull place', pathImage:'asset/img/profile_pic4'),
+      ],
+    );
+  }
+
+}
